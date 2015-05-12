444a500f6646d942c6af0767187010d324044b76
thomec <thomec@mailbox.org>
2015-05-12 20:38:59 +0200

    mysql setup

--- tox/tox/settings.py
+++ tox/tox/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'tox.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.backends.mysql', # 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pepe',
+        'USER': 'pepe',
+        'PASSWORD': 'pepe',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
