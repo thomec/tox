83f0452b739ad38131854c48036681f5c8ff4c63
Thomas Müller <thomec@gmail.com>
2015-10-04 18:16:27 +0200

    rango: Syntax error in forms.PageForm behoben

--- rango/forms.py
+++ rango/forms.py
@@ -33,8 +33,8 @@ class PageForm(forms.ModelForm):
         cleaned_data = self.cleaned_data
         url = cleaned_data.get('url')
 
-        if url and not url.startswith('http://')
-                and not url.startswith('https://'):
+        if url and not (url.startswith('http://')
+            or url.startswith('https://')):
             url = 'http://' + url
             cleaned_data['url'] = url
 
